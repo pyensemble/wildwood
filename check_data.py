dab104739f6e2dc16dcb553b4efd8e7802f95dae
yiyang <38939225+yiyang-yu@users.noreply.github.com>
2021-04-26 17:50:14 +0200

    minimal example to run hyperopt
    inspired by catboost experiments

--- check_data.py
+++ check_data.py
@@ -7,4 +7,14 @@ print(dataset.df_raw)
 
 X_train, X_test, y_train, y_test = dataset.extract(42)
 
-print(dataset.columns_)
+# print(dataset.columns_)
+from experiments.experiment import LGBExperiment
+
+learning_task = 'classification'
+n_estimators = 100
+
+exp = LGBExperiment(learning_task, n_estimators=n_estimators, max_hyperopt_evals=2)
+default_cv_result = exp.run_cv([((X_train, y_train), (X_test, y_test))])  # TODO: shoud be [((X_train, y_train), (X_val, y_val))]
+tuned_cv_result = exp.optimize_params([((X_train, y_train), (X_test, y_test))], max_evals=2)
+# TODO: add fitting time
+
