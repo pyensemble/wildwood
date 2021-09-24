cbab59c54900ab7c1083d14df49530f20e15580a
stephanegaiffas <stephane.gaiffas@gmail.com>
2021-09-24 13:25:45 +0200

    Passed to v0.2

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@ copyright = "2020, Stéphane Gaïffas"
 author = "Stéphane Gaïffas"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1"
+release = "0.2"
 
 # -- General configuration ---------------------------------------------------
 
