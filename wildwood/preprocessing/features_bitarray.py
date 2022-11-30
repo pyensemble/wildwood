74f311e1e7694b91360ee0c4bf2d7de5f9699065
Merad <merad@MacBook-Pro-de-Merad.local>
2022-11-30 12:50:19 +0100

    debugging featuresbitarray zero division

--- wildwood/preprocessing/features_bitarray.py
+++ wildwood/preprocessing/features_bitarray.py
@@ -129,9 +129,13 @@ class FeaturesBitArray(object):
             else:
                 self.n_bits[j] = ceil(np.log2(max_value + 1))
                 self.n_values_in_words[j] = floor(64 / self.n_bits[j])
+                if self.n_bits[j] == 0:
+                    print("n_bits is zero for j = %d" % j)
                 self.bitmasks[j] = (1 << self.n_bits[j]) - 1
 
             n_words = ceil(n_samples / self.n_values_in_words[j])
+            if self.n_values_in_words[j] == 0:
+                print("n_values is zero for j = %d" % j)
             offset += n_words
             self.offsets[j + 1] = offset
 
