3db1a5fe1b8b002fd0ce5d7196a51392f95d659a
Stephane Gaiffas <stephanegaiffas@users.noreply.github.com>
2023-11-15 15:49:22 +0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@ copyright = "2020, Stéphane Gaïffas"
 author = "Stéphane Gaïffas"
 
 # The full version, including alpha/beta/rc tags
-release = "0.2"
+release = "0.3"
 
 # -- General configuration ---------------------------------------------------
 
